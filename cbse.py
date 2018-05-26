86ef4d1b9f58a973a3ee82258e17c5729985bcaa
Amal Pavithran <amal110100@gmail.com>
2018-05-26 17:59:54 +0530

    Update cbse.py

--- cbse.py
+++ cbse.py
@@ -7,15 +7,18 @@ driver.get('http://cbseresults.nic.in/class12zpq/class12th18.htm')
 filepath = 'marks.txt'
 school_number = '72511'
 center_number = '8801'
-rollnumber = 8100000  #to be iterated
+rollnumber = 9100001  #to be iterated
 
 f = open('marks.txt', 'r')
 x = f.read().splitlines()
 
-roll_box = driver.find_element_by_name("regno").send_keys(roll_number)
-
-school_box = driver.find_element_by_name("sch").send_keys(school_number)
-
-center_box = driver.find_element_by_name("cno").send_keys(center_number)
-
-sumbmit_button = driver.find_element_by_name("B2")
+for i in range(800):
+  #Enter roll
+  roll_box = driver.find_element_by_name("regno").send_keys(roll_number+i)
+  #Enter school number
+  school_box = driver.find_element_by_name("sch").send_keys(school_number)
+  #Enter center number
+  center_box = driver.find_element_by_name("cno").send_keys(center_number)
+  #Press submit
+  sumbmit_button = driver.find_element_by_name("B2")
+  
