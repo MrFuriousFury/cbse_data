76974c3ff28581647e84c4685c0b5fe935ce45cf
Amal Pavithran <amal110100@gmail.com>
2018-05-26 19:06:03 +0530

    Update cbse.py

--- cbse.py
+++ cbse.py
@@ -1,3 +1,4 @@
+#from openpyxl import load_workbook
 from selenium import webdriver
 import time
 
@@ -8,8 +9,18 @@ school_number = '72511'
 center_number = '8801'
 roll_number = 9100001  #to be iterated
 
+#for i in range(800)
 roll_box = driver.find_element_by_name("regno").send_keys(roll_number)
 school_box = driver.find_element_by_name("sch").send_keys(school_number)
 center_box = driver.find_element_by_name("cno").send_keys(center_number)
 sumbmit_button = driver.find_element_by_name("B2")
 sumbmit_button.click()
+time.sleep(10)
+name = driver.find_element_by_xpath("/html/body/div[1]/table[1]/tbody/tr[2]/td[2]/font/b").text
+print(name)
+for i in range(2,7):
+    sub = driver.find_element_by_xpath("/html/body/div[1]/div/center/table/tbody/tr[" + i + "]/td[2]/font").text
+    marks = driver.find_element_by_xpath("/html/body/div[1]/div/center/table/tbody/tr[" + i + "]/td[5]/font").text
+    print(i+": "+sub)
+    print(i+": "+marks)
+driver.quit()
